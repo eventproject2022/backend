03d237df2e86d523e70c4bb09cd0691cb207eb04
Rizwan <104551910+RizwanSidd@users.noreply.github.com>
2022-07-20 16:36:27 +0530

    Update user.controller.js

--- _controllers/user.controller.js
+++ _controllers/user.controller.js
@@ -5,32 +5,24 @@ const jsonwebtoken = require('jsonwebtoken');
 exports.userSignUp = (req, res) => {
     user.findOne({ email: req.body.email }).then(userFound => {
         if (userFound == null) {
-            user.findOne({ phone: req.body.phone }).then(userFound => {
-                if (userFound == null) {
-                    bcryptjs.hash(req.body.password, 10).then((hashed) => {
-                        let ins = new user({
-                            name: req.body.name,
-                            email: req.body.email,
-                            password: hashed,
-                            phone: req.body.phone,
-                            address: req.body.address,
-                            state: req.body.state,
-                            country: req.body.country,
-                        });
-                        ins.save().then((created) => {
-                            if (created) {
-                                res.status(200).json({ err: false, msg: "User Signup successfully." })
-                            } else {
-                                res.status(500).json({ err: true, msg: err })
-                            }
-                        })
-                    }).catch((err) => {
+            bcryptjs.hash(req.body.password, 10).then((hashed) => {
+                let ins = new user({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: hashed,
+                    phone: req.body.phone,
+                    address: req.body.address,
+                    state: req.body.state,
+                    country: req.body.country,
+                });
+                ins.save().then((created) => {
+                    if (created) {
+                        res.status(200).json({ err: false, msg: "User Signup successfully." })
+                    } else {
                         res.status(500).json({ err: true, msg: err })
-                    })
-                } else {
-                    res.status(500).json({ err: true, msg: "Phone number is already exists." });
-                }
-            }).catch(err => {
+                    }
+                })
+            }).catch((err) => {
                 res.status(500).json({ err: true, msg: err })
             });
         } else {
@@ -44,7 +36,7 @@ exports.userLogin = (req, res) => {
     user.findOne({ email: req.body.email }).then((found) => {
         bcryptjs.compare(req.body.password, found.password).then((compared) => {
             if (compared == true) {
-                let token = jsonwebtoken.sign({ _id: found._id }, 'privateKey');
+                let token = jsonwebtoken.sign({ _id: found._id, name: found.name, email: found.email }, 'privateKey');
                 res.status(200).json({ err: false, msg: "User Login successfully.", token: token });
             } else {
                 res.status(500).json({ err: true, msg: "Password is incorrect." });
